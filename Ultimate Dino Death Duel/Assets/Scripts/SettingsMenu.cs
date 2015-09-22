ec0849131c21a5ce1585f558b2a7a1e72fa477d4
Mike Plouffe <mike@mike-plouffe.com>
2015-09-21 20:15:16 -0600

    Added Logo, Fixed settings
    +Logo displays at home screen
    +settings fixed again!
    >previously, onEnable was setting music toggle to master toggle value

--- Ultimate Dino Death Duel/Assets/Scripts/SettingsMenu.cs
+++ Ultimate Dino Death Duel/Assets/Scripts/SettingsMenu.cs
@@ -101,8 +101,8 @@ namespace DinoDuel
 			SFX_Level = userSettings.SFX_Level;
 			VO_Toggle = userSettings.VO_Toggle;
 			VO_Level = userSettings.VO_Level;
-			MUS_Toggle = userSettings.Master_Toggle;
-			MUS_Level = userSettings.Master_Level;
+			MUS_Toggle = userSettings.MUS_Toggle;
+			MUS_Level = userSettings.MUS_Level;
 			Damage_Toggle = userSettings.Damage_Toggle;
 			Push_Toggle = userSettings.Push_Toggle;
 		}
